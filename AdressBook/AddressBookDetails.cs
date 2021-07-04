0d6f6ce58014a02008a16d61418e9ef487a30a4d
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-04 20:45:40 +0530

    [Girish]Edited the details of existing contacts

--- AdressBook/AddressBookDetails.cs
+++ AdressBook/AddressBookDetails.cs
@@ -63,6 +63,89 @@ namespace AdressBook
             Console.WriteLine($"Phone Number: {person.phoneNumber}\n");
             Console.WriteLine($"Email id: {person.email}");
         }
+        public static void EditDetails()
+        {
+            int f;
+            if (People.Count > 0)
+            {
+                Console.Write("Enter name of a person you want to edit: ");
+                string editName = Console.ReadLine();
+
+                foreach (var person in People)
+                {
+                    if (editName.ToLower() == person.firstName.ToLower())
+                    {
+                        while (true)
+                        {
+                            f = 0;
+                            Console.WriteLine("1.First name\n2.Last name\n3.Address\n4.City\n5.State\n6.ZipCode\n7.Phone Number\n8.email\n9.Exit");
+                            Console.WriteLine("Enter Option You want to edit");
+                            switch (Convert.ToInt32(Console.ReadLine()))
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter New First name");
+                                    person.firstName = Console.ReadLine();
+                                    Console.WriteLine("First name modified");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter New Last name");
+                                    person.lastName = Console.ReadLine();
+                                    Console.WriteLine("Last Name Modified");
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter New Address");
+                                    person.address = Console.ReadLine();
+                                    Console.WriteLine("Address mModified");
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter New City");
+                                    person.city = Console.ReadLine();
+                                    Console.WriteLine("City Modified");
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Enter New State");
+                                    person.state = Console.ReadLine();
+                                    Console.WriteLine("State Modified");
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Enter New Zip Code");
+                                    person.zipCode = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine("Zip code Modified");
+                                    break;
+                                case 7:
+                                    Console.Write("Enter new Phone Number: ");
+                                    string phNo = Console.ReadLine();
+                                    person.phoneNumber = phNo;
+                                    Console.WriteLine("Phone Number Modified");
+                                    break;
+                                case 8:
+                                     Console.Write("Enter new Email-id: ");
+                                     string emailId = Console.ReadLine();
+                                     Console.WriteLine("Email id  Modified");
+                                    break;
+                                case 9:
+                                    Console.WriteLine("Exited");
+                                    f = 1;
+                                    break;
+
+                            }
+                            if (f == 1)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entered name is not in Contact list");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your contact list is empty");
+            }
+        }
         public static void DeleteDetails()
         {
             int f = 0;
@@ -75,7 +158,6 @@ namespace AdressBook
                 {
                     if (deleteName.ToLower() == person.firstName.ToLower())
                     {
-                        //removing from list
                         Console.WriteLine($"You have deleted {person.firstName} contact");
                         People.Remove(person);
                         f = 1;
--- AdressBook/Program.cs
+++ AdressBook/Program.cs
@@ -10,7 +10,7 @@ namespace AdressBook
 
             while (true)
             {
-                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List \n3.Delete members in Contact list \n4.Exit");
+                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List \n3.Delete members in Contact list \n4.Edit existing contact in List\n5.Exit");
                 Console.WriteLine("\nEnter an option:");
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
@@ -24,6 +24,9 @@ namespace AdressBook
                         AddressBookDetails.DeleteDetails();
                         break;
                     case 4:
+                        AddressBookDetails.EditDetails();
+                        break;
+                    case 5:
                         // to exit from main method
                         return;
                 }
